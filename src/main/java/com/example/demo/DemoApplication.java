a653b38476671a1ad670f923a3a1988278104187
Muhammed Kılıç <>
2020-08-13 17:33:00 +0300

    Update DemoApplication.java
    Path variable usage: /hello/2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,8 @@
 package com.example.demo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
@@ -17,5 +16,11 @@ public class DemoApplication {
     @GetMapping("/hello")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
+    }
+
+    @RequestMapping(value = "/hello/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String sayUmbrella(@PathVariable final String id) {
+        return "Yo soy numero " + id;
     }
 }
